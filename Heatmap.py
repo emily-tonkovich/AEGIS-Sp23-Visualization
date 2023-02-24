1749648ccc113b11c943d47bccf31e2273eaf950
jffshao <jffshao@gmail.com>
2023-02-24 14:26:17 -0500

    Update Heatmap.py

--- Heatmap.py
+++ Heatmap.py
@@ -1,34 +1,23 @@
-from bokeh.models import LinearColorMapper, ColorBar
-from bokeh.transform import transform
-from bokeh.plotting import figure,show
-import pandas as pd
- 
-# Creating a set of data in d
-d={'Col0':[ 190, 320, 270, 874, 459, 124, 546,
-           285, 341, 980, 1002, 453, 324, 245],
-   'Col1':[ 71, 128, 34, 49, 52, 87, 78, 25, 67,
-           19, 34, 100, 287, 55],
-   'Col2':[ 1123, 6471, 8345, 3253, 6420, 1830,
-           7849, 2937, 2108, 5392, 1273, 3928, 4927, 7392]}
- 
-# Set of data into a dataframe 
- 
-df = pd.DataFrame(d)
- 
-# Color bar template and minimum and maximum values
-color = LinearColorMapper(palette = 'Turbo256', low = df.Col0.min(), high = df.Col0.max())
- 
-# Figure with specified axes 
-colorbar = figure(plot_width = 750, plot_height = 600, x_axis_label = 'Column 1', y_axis_label = 'Column 2')
- 
-# Plotting the points with defined size and opacity
-colorbar.circle(x = 'Col1', y = 'Col2', source = df, color = transform('Col0', color), size = 15, alpha = 0.5)
- 
-# Location in the plot
-color_bar = ColorBar(color_mapper = color, label_standoff = 14, location = (0,0), title = 'Plot')
- 
-# Defining the position of the color bar
-colorbar.add_layout(color_bar, 'right')
- 
-# Showing the above implementation
-show(colorbar)
+import plotly.graph_objects as go
+
+import numpy as np
+
+fig = go.Figure(data =
+    go.Contour(
+        
+z =   [[2, 4, 7, 12, 13, 14, 15, 16],
+       [3, 1, 6, 11, 12, 13, 16, 17],
+       [4, 2, 7, 7, 11, 14, 17, 18],
+       [5, 3, 8, 8, 13, 15, 18, 19],
+       [7, 4, 10, 9, 16, 18, 20, 19],
+       [9, 10, 5, 27, 23, 21, 21, 21],
+       [11, 14, 17, 26, 25, 24, 23, 22]],
+        dx=10,
+        x0=5,
+        dy=10,
+        y0=10,
+        line_smoothing=0.95
+    )
+)
+
+fig.show()
